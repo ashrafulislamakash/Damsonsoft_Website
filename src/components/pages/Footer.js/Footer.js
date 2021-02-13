4f6c599aafb39122edb3feb82c70d971b630120e
ashrafulislamakash <ashrafulislam.eng.com>
2021-02-13 11:24:48 +0600

    code cleenup

--- src/components/pages/Footer.js/Footer.js
+++ src/components/pages/Footer.js/Footer.js
@@ -76,7 +76,7 @@ function Footer() {
               Damsonsoft
             </Link>
           </div>
-          <small className="website-rights">{""}© Copyright 2020</small>
+          <small className="website-rights">{""}© Copyright 2021</small>
           <div className="social-icons">
             <Link
               className="social-icon-link"
